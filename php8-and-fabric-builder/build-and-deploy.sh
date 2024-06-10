fc77898a639b0e325af312657467fd3498f10830
Luis Lopes <git@luislopes.org>
2024-06-10 17:59:51 +0200

    php versions as build tags

--- php8-and-fabric-builder/build-and-deploy.sh
+++ php8-and-fabric-builder/build-and-deploy.sh
@@ -2,5 +2,14 @@
 
 set -e
 
-docker build -t lfglopes/php8-and-fabric-builder:latest .
-docker push lfglopes/php8-and-fabric-builder:latest
+# Get PHP version as an argument. If no argument present, use 8.1 as default
+PHP_VERSION=${1:-8.1}
+
+docker build --build-arg PHP_VERSION=$PHP_VERSION -t lfglopes/php8-and-fabric-builder:$PHP_VERSION .
+docker push lfglopes/php8-and-fabric-builder:$PHP_VERSION
+
+
+if [ "$PHP_VERSION" = "8.1" ]; then
+    docker tag lfglopes/php8-and-fabric-builder:latest lfglopes/php8-and-fabric-builder:8.1
+    docker push lfglopes/php8-and-fabric-builder:latest
+fi
