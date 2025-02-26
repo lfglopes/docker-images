cd9811f34cff5bafbebe5ec19ed4ff6e65264d17
Luis Lopes <git@luislopes.org>
2025-02-26 19:17:02 +0100

    update some packages to php 8.2

--- php8-and-fabric-builder/build-and-deploy.sh
+++ php8-and-fabric-builder/build-and-deploy.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-# Get PHP version as an argument. If no argument present, use 8.1 as default
-PHP_VERSION=${1:-8.1}
+# Get PHP version as an argument. If no argument present, use 8.2 as default
+PHP_VERSION=${1:-8.2}
 
 docker build --build-arg PHP_VERSION=$PHP_VERSION -t lfglopes/php8-and-fabric-builder:$PHP_VERSION .
 docker push lfglopes/php8-and-fabric-builder:$PHP_VERSION
 
 
-if [ "$PHP_VERSION" = "8.1" ]; then
-    docker tag lfglopes/php8-and-fabric-builder:latest lfglopes/php8-and-fabric-builder:8.1
+if [ "$PHP_VERSION" = "8.2" ]; then
+    docker tag lfglopes/php8-and-fabric-builder:latest lfglopes/php8-and-fabric-builder:8.2
     docker push lfglopes/php8-and-fabric-builder:latest
 fi
